0dea7548831239e0a4ea7444a2541d7c6cd09fd6
adam.kurek <adam.kurek@digital.cz>
2020-04-23 19:49:35 +0200

    [FIX] make it the f*k work!

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelCase'
+import { camelCase } from 'lodash'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import { terser } from "rollup-plugin-terser";
